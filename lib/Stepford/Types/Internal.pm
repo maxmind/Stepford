9d309689a233169c4f79e8f2680d5c57a55c46d4
Dave Rolsky <autarch@urth.org>
2014-02-18 10:56:57 -0600

    Only require 1+ file for ArrayOfFiles

--- lib/Stepford/Types/Internal.pm
+++ lib/Stepford/Types/Internal.pm
@@ -21,7 +21,7 @@ subtype ArrayOfDependencies, as ArrayRef [NonEmptySimpleStr];
 coerce ArrayOfDependencies, from NonEmptySimpleStr, via { [$_] };
 
 subtype ArrayOfFiles, as ArrayRef [File], inline_as {
-    $_[0]->parent()->_inline_check( $_[1] ) . " && \@{ $_[1] } > 1";
+    $_[0]->parent()->_inline_check( $_[1] ) . " && \@{ $_[1] } >= 1";
 };
 
 coerce ArrayOfFiles, from File, via { [$_] };
