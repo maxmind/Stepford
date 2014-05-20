2550668f5a5b37a6e7fe1bd09f2c55d794f0c37c
Dave Rolsky <autarch@urth.org>
2014-05-20 17:52:57 -0500

    Make sure step sets are always sorted for predictability

--- lib/Stepford/Plan.pm
+++ lib/Stepford/Plan.pm
@@ -70,7 +70,7 @@ sub _build_step_sets {
             }
         );
 
-        push @sets, [ uniq map { $_->node() } @leaves ];
+        push @sets, [ sort uniq map { $_->node() } @leaves ];
 
         for my $leaf (@leaves) {
             my $parent = $leaf->parent();
